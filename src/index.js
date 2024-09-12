81e0d054da494fde7c1091306eaddc74c960685a
abhishek.gu <abhishek.gu@neurlit.com>
2024-09-12 20:34:25 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import SignIn from './Component/SignIn';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
